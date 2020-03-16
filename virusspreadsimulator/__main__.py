2b7cd38aed7858af37f617a1d18847bede5bd5ee
PhilippRichter <philipp.richter@gmail.com>
2020-03-16 18:16:38 +0100

    added __main__.py to launch application

---
+++ virusspreadsimulator/__main__.py
@@ -0,0 +1,8 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+logger.info('Launched through __main__.py')
