c320495f862b5fb07723ebeb79b2cd57997a701f
PhilippRichter <philipp.richter@gmail.com>
2020-03-17 12:59:57 +0100

    changed data loading from remote to local

--- virusspreadsimulator/prototype_philipp.py
+++ virusspreadsimulator/prototype_philipp.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import logging
-from typing import List
+from typing import List, TextIO
+import importlib.resources as pkg_resources
+import data  # relative-import the *package* containing the data
 
 
 def main() -> None:
@@ -11,23 +13,22 @@ def main() -> None:
         'ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Lat', 'Long', 'Alt',
         'Timezone', 'DST', 'Tz database time zone', 'type', 'source'
     ]  # type: List[str]
-    airport_df = pd.read_csv(
-        "https://raw.githubusercontent.com/PhilippRichter/VirusSpreadSimulator/master/virusspreadsimulator/data/airports.dat",
-        names=airport_col,
-        index_col=0)  # type: pd.DataFrame
-    logger.info('Downloaded airports.dat')
+    airports_path = pkg_resources.open_text(data, 'airports.dat')  # type: TextIO
+    airport_df = pd.read_csv(airports_path, names=airport_col,
+                             index_col=0)  # type: pd.DataFrame
+    logger.info('Loaded airports.dat')
 
     # download flight routes data
     route_cols = [
         'Airline', 'Airline ID', 'Source Airport', 'Source Airport ID',
         'Dest Airport', 'Dest Airport ID', 'Codeshare', 'Stops', 'equipment'
     ]  # type: List[str]
-    routes_df = pd.read_csv(
-        "https://raw.githubusercontent.com/PhilippRichter/VirusSpreadSimulator/master/virusspreadsimulator/data/routes.dat",
-        names=route_cols)  # type: pd.DataFrame
-    logger.info('Downloaded routes.dat')
+    flights_path = pkg_resources.open_text(data, 'routes.dat')  # type: TextIO
+    routes_df = pd.read_csv(flights_path,
+                            names=route_cols)  # type: pd.DataFrame
+    logger.info('Loaded routes.dat')
 
-    # clean up data, change 'object' type to numeric and drops NaNs
+    # clean up data, change 'object' type to numeric and drop NaNs
     routes_df['Source Airport ID'] = pd.to_numeric(
         routes_df['Source Airport ID'].astype(str), 'coerce')
     routes_df['Dest Airport ID'] = pd.to_numeric(
